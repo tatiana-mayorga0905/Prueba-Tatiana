300394fee571b32fbaa19d503b299d12dbe77020
tatiana-mayorga0905 <tmayorga@cuevatech.com>
2026-02-09 12:07:19 -0500

    test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -3,10 +3,11 @@ import { LoginPage } from '../pages/LoginPage';
 
 test.describe('Login', () => {
 
-  test( 'Login with valid credentials',
+  test('Login with valid credentials',
     { tag: ['@dev', '@qa', '@app', '@smoke', '@login'] },
     
     async ({ page }, testInfo) => {
+
       testInfo.annotations.push({
         type: 'test_key',
         description: 'XSP-56'
@@ -15,10 +16,18 @@ test.describe('Login', () => {
       const env = process.env.TARGET_ENV || 'dev';
       const loginPage = new LoginPage(page);
 
-      await page.goto('/');
-      await loginPage.login(env);
+      await test.step('Abrir la aplicación', async () => {
+        await page.goto('/');
+      });
 
-      await expect(page).not.toHaveURL(/login/);
+      await test.step('Iniciar login con credenciales válidas', async () => {
+        await loginPage.login(env);
+      });
+
+      await test.step('Validar que el usuario ingresó correctamente', async () => {
+        await expect(page).not.toHaveURL(/login/);
+      });
+
     }
   );
 
